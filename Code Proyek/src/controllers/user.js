4ab6b02c94b61fd43917cb742b15c08494e79c0e
Kennetyjr <kennetyjr@gmail.com>
2023-05-17 20:33:40 +0700

    controller register, login, database user

--- Code Proyek/src/controllers/user.js
+++ Code Proyek/src/controllers/user.js
@@ -1,62 +1,132 @@
 const register = async (req, res) => {
+  var cek = Joi.object({
+    username: Joi.string().required(),
+    password: Joi.string().required(),
+    gender: Joi.string().valid("F", "M").required().toUpperCase(),
+    birth_date: Joi.date().format("DD/MM/YYYY"),
+    body_weight: Joi.string().required(),
+    body_height: Joi.string().required(),
+  });
+  try {
+    await cek.validateAsync(req.body);
 
+    var username = req.body.username;
+    var password = req.body.password;
+    var gender = req.body.gender;
+    var birth_date = req.body.birth_date;
+    var body_weight = req.body.body_weight;
+    var body_height = req.body.body_height;
+
+    var [cekuser] = await sequelize.query(
+      "select * from user where username = '" + username + "'"
+    );
+    if (cekuser.length == 0) {
+      if (gender == "MALE" || gender == "FEMALE") {
+        var alphabet =
+          "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        var api = "";
+        for (var i = 0; i < 10; i++) {
+          var acak = Math.floor(Math.random() * 62);
+          var hrf = alphabet.substr(acak, 1);
+          api = api + hrf;
+        }
+        var api_hit = 0;
+        var saldo = 0;
+        var userbaru = mod_users.build(
+          username,
+          password,
+          gender,
+          birth_date,
+          body_weight,
+          body_height,
+          saldo,
+          api_hit,
+          api
+        );
+
+        await userbaru.save();
+        res.json({
+          message: "User created successfully.",
+          data: newUser,
+        });
+      } else {
+        res.status(400).send({ msg: "Gender harus MALE/FEMALE" });
+      }
+    } else {
+      res.status(400).send({ msg: "Akun Sudah Terdaftar" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const login = async (req, res) => {
-    
+  var cek = Joi.object({
+    username: Joi.string().required(),
+    password: Joi.string().required(),
+  });
+  try {
+    await cek.validateAsync(req.body);
+
+    var username = req.body.username;
+    var password = req.body.password;
+
+    var [cekuser] = await sequelize.query(
+      "select * from user where username = '" + username + "'"
+    );
+    if (cekuser.length > 0) {
+      if (cekuser[0].username == username && cekuser[0].password == password) {
+        return res.status(200).json({
+          username: username,
+          gender: cekuser[0].gender,
+          birth_date: cekuser[0].birth_date,
+          body_weight: cekuser[0].body_weight,
+          body_height: cekuser[0].body_height,
+          saldo: cekuser[0].saldo,
+          api_hit: cekuser[0].api_hit,
+        });
+      } else {
+        res.status(400).send({ msg: "Username atau Password salah" });
+      }
+    } else {
+      res.status(400).send({ msg: "Akun tidak terdaftar" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const updateUserData = async (req, res) => {
-    
-};
+const updateUserData = async (req, res) => {};
 
-const updatePassword = async (req, res) => {
-    
-};
+const updatePassword = async (req, res) => {};
 
-const topup = async (req, res) => {
-    
-};
+const topup = async (req, res) => {};
 
-const recharge = async (req, res) => {
-    
-};
+const recharge = async (req, res) => {};
 
-const diet = async (req, res) => {
-    
-};
+const diet = async (req, res) => {};
 
-const calculateBMI = async (req, res) => {
-    
-};
+const calculateBMI = async (req, res) => {};
 
-const scheduleDiet = async (req, res) => {
-    
-};
+const scheduleDiet = async (req, res) => {};
 
-const getSchedule = async (req, res) => {
-    
-};
+const getSchedule = async (req, res) => {};
 
-const updateSchedule = async (req, res) => {
-    
-};
+const updateSchedule = async (req, res) => {};
 
-const getCalories = async (req, res) => {
-    
-};
+const getCalories = async (req, res) => {};
 
 module.exports = {
-    register,
-    login,
-    updateUserData,
-    updatePassword,
-    topup,
-    recharge,
-    diet,
-    calculateBMI,
-    scheduleDiet,
-    getSchedule,
-    updateSchedule,
-    getCalories,
+  register,
+  login,
+  updateUserData,
+  updatePassword,
+  topup,
+  recharge,
+  diet,
+  calculateBMI,
+  scheduleDiet,
+  getSchedule,
+  updateSchedule,
+  getCalories,
 };
--- proyek_ws.sql
+++ proyek_ws.sql
@@ -1,24 +1,31 @@
-/*
-SQLyog Professional v13.1.1 (64 bit)
-MySQL - 10.4.22-MariaDB : Database - proyek_ws
-*********************************************************************
-*/
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 17 Bulan Mei 2023 pada 15.30
+-- Versi server: 10.4.22-MariaDB
+-- Versi PHP: 8.1.2
 
-/*!40101 SET NAMES utf8 */;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-/*!40101 SET SQL_MODE=''*/;
 
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`proyek_ws` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-USE `proyek_ws`;
+--
+-- Database: `proyek_ws`
+--
 
-/*Table structure for table `menu_sets` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `menu_sets`;
+--
+-- Struktur dari tabel `menu_sets`
+--
 
 CREATE TABLE `menu_sets` (
   `menu_set_id` varchar(255) NOT NULL,
@@ -27,19 +34,55 @@ CREATE TABLE `menu_sets` (
   `menu_set_total_calories` int(255) NOT NULL,
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL,
-  `deletedAt` datetime NOT NULL,
-  PRIMARY KEY (`menu_set_id`)
+  `deletedAt` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-/*Data for the table `menu_sets` */
+--
+-- Dumping data untuk tabel `menu_sets`
+--
 
-insert  into `menu_sets`(`menu_set_id`,`menu_set_name`,`menu_content`,`menu_set_total_calories`,`createdAt`,`updatedAt`,`deletedAt`) values 
-('MS001','random','[{\"menuId\":192386,\"menuName\":\"Pizza Buddy Pizza Dough, 16 oz\",\"menuCalories\":113},{\"menuId\":424571,\"menuName\":\"Bacon King Burger\",\"menuCalories\":1040},{\"menuId\":419357,\"menuName\":\"Burger Sliders\",\"menuCalories\":1300}]',2453,'2023-05-17 06:14:19','2023-05-17 06:14:19','0000-00-00 00:00:00'),
-('MS002','salad-salad an','[{\"menuId\":273066,\"menuName\":\"Salad Ole\",\"menuCalories\":466.3999938964844},{\"menuId\":351155,\"menuName\":\"Salad Bar, Kidney Beans\",\"menuCalories\":110},{\"menuId\":384649,\"menuName\":\"Fat Salad Wedge (no dressing)\",\"menuCalories\":60}]',636,'2023-05-17 06:30:23','2023-05-17 06:30:23','0000-00-00 00:00:00'),
-('MS003','ikan-ikan an','[{\"menuId\":341757,\"menuName\":\"Meyer Lemon Mussels & Frites\",\"menuCalories\":1970},{\"menuId\":227623,\"menuName\":\"Pecan Parmesan Crusted Rainbow Trout\",\"menuCalories\":793},{\"menuId\":354792,\"menuName\":\"Atlantic Salmon (Regular)\",\"menuCalories\":490}]',6203,'2023-05-17 06:39:45','2023-05-17 06:39:45','0000-00-00 00:00:00'),
-('MS004','test set','[{\"menuId\":341757,\"menuName\":\"Meyer Lemon Mussels & Frites\",\"menuCalories\":1970},{\"menuId\":227623,\"menuName\":\"Pecan Parmesan Crusted Rainbow Trout\",\"menuCalories\":793},{\"menuId\":354792,\"menuName\":\"Atlantic Salmon (Regular)\",\"menuCalories\":490}]',6203,'2023-05-17 10:19:44','2023-05-17 10:19:44','0000-00-00 00:00:00');
+INSERT INTO `menu_sets` (`menu_set_id`, `menu_set_name`, `menu_content`, `menu_set_total_calories`, `createdAt`, `updatedAt`, `deletedAt`) VALUES
+('MS001', 'random', '[{\"menuId\":192386,\"menuName\":\"Pizza Buddy Pizza Dough, 16 oz\",\"menuCalories\":113},{\"menuId\":424571,\"menuName\":\"Bacon King Burger\",\"menuCalories\":1040},{\"menuId\":419357,\"menuName\":\"Burger Sliders\",\"menuCalories\":1300}]', 2453, '2023-05-17 06:14:19', '2023-05-17 06:14:19', '0000-00-00 00:00:00'),
+('MS002', 'salad-salad an', '[{\"menuId\":273066,\"menuName\":\"Salad Ole\",\"menuCalories\":466.3999938964844},{\"menuId\":351155,\"menuName\":\"Salad Bar, Kidney Beans\",\"menuCalories\":110},{\"menuId\":384649,\"menuName\":\"Fat Salad Wedge (no dressing)\",\"menuCalories\":60}]', 636, '2023-05-17 06:30:23', '2023-05-17 06:30:23', '0000-00-00 00:00:00'),
+('MS003', 'ikan-ikan an', '[{\"menuId\":341757,\"menuName\":\"Meyer Lemon Mussels & Frites\",\"menuCalories\":1970},{\"menuId\":227623,\"menuName\":\"Pecan Parmesan Crusted Rainbow Trout\",\"menuCalories\":793},{\"menuId\":354792,\"menuName\":\"Atlantic Salmon (Regular)\",\"menuCalories\":490}]', 6203, '2023-05-17 06:39:45', '2023-05-17 06:39:45', '0000-00-00 00:00:00'),
+('MS004', 'test set', '[{\"menuId\":341757,\"menuName\":\"Meyer Lemon Mussels & Frites\",\"menuCalories\":1970},{\"menuId\":227623,\"menuName\":\"Pecan Parmesan Crusted Rainbow Trout\",\"menuCalories\":793},{\"menuId\":354792,\"menuName\":\"Atlantic Salmon (Regular)\",\"menuCalories\":490}]', 6203, '2023-05-17 10:19:44', '2023-05-17 10:19:44', '0000-00-00 00:00:00');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `username` varchar(20) NOT NULL,
+  `password` varchar(10) DEFAULT NULL,
+  `gender` varchar(6) DEFAULT NULL,
+  `birth_date` varchar(10) DEFAULT NULL,
+  `body_weight` int(5) DEFAULT NULL,
+  `body_height` int(5) DEFAULT NULL,
+  `saldo` int(10) DEFAULT NULL,
+  `api_hit` int(15) DEFAULT NULL,
+  `api_key` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`username`, `password`, `gender`, `birth_date`, `body_weight`, `body_height`, `saldo`, `api_hit`, `api_key`) VALUES
+('akun1', '123', 'MALE', '11/09/2001', 60, 175, 20000, 3, 'bNEcmQd8rQ');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `menu_sets`
+--
+ALTER TABLE `menu_sets`
+  ADD PRIMARY KEY (`menu_set_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
