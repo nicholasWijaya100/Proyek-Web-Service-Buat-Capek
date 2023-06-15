c6b074e83f1c72ed14225ca5675fcd0687520e84
MarioHoliwono <90135814+MarioHoliwono@users.noreply.github.com>
2023-06-15 20:43:10 +0700

    seeder jalan

--- Code Proyek/src/seeders/20230510020245-users_seed.js
+++ Code Proyek/src/seeders/20230510020245-users_seed.js
@@ -3,45 +3,47 @@
 const { faker } = require("@faker-js/faker");
 const penggunas = [];
 
-for (let i = 0; i < 100; i++) {
-  const namaDepan = faker.name.firstName();
-  const namaBelakang = faker.name.lastName();
+for (let i = 0; i < 50; i++) {
+  const namaDepan = faker.person.firstName();
+  const namaBelakang = faker.person.lastName();
   const namaLengkap = `${namaDepan} ${namaBelakang}`;
   const chars =
   "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   const generatedStrings = [];
-  let pass;
+  let apiKey;
   do {
-    pass = "";
+    apiKey = "";
     for (let i = 0; i < 7; i++) {
-      pass += chars.charAt(Math.floor(Math.random() * chars.length));
+      apiKey += chars.charAt(Math.floor(Math.random() * chars.length));
     }
-  } while (generatedStrings.includes(pass));
-  generatedStrings.push(pass);
+  } while (generatedStrings.includes(apiKey));
+  generatedStrings.push(apiKey);
 
   const height = Math.floor(Math.random() * (200 - 150 + 1)) + 150
+  const weight = Math.floor(Math.random() * (150 - 50 + 1)) + 50
 
   let target = 0
   do{
-    target = Math.floor(Math.random() * (200 - 150 + 1)) + 150
-  }while(target < height && target != height)
+    target = Math.floor(Math.random() * (160 - 50 + 1)) + 60
+  }while(target < weight)
 
   penggunas.push({
     username: namaLengkap,
-    password: pass,
-    gender: faker.helpers.arrayElement(['P', 'L']),
-    birthdate: faker.date.birthdate({min: 1990, max:2005}),
-    body_weight: Math.floor(Math.random() * (150 - 50 + 1)) + 50,
+    password: '123',
+    gender: faker.helpers.arrayElement(['MALE', 'FEMALE']),
+    birth_date: faker.date.birthdate({min: 1990, max:2005}),
+    body_weight: weight,
     body_height: height,
-    target_height: target,
-    is_vegetarian: faker.helpers.arrayElement(['Y', 'N']),
-    profile_picture: 'a',
+    target_weight: target,
+    role: faker.helpers.arrayElement(['consultant', 'user']),
+    saldo: 0,
+    api_hit: 0,
+    api_key: apiKey,
+    profile_picture: `${namaLengkap}.jpg`,
     createdAt: new Date(),
     updatedAt: new Date(),
   });
 }
-
-
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
